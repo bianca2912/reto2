a0991aea168c57d441dd5383f10ea8e6682db1a7
bianca2912 <biancasimina01@gmail.com>
2024-10-31 10:08:33 +0100

    mi parte

--- reto2/src/reto2/funciones1.java
+++ reto2/src/reto2/funciones1.java
@@ -4,18 +4,95 @@ import java.util.Scanner;
 
 public class funciones1 {
 
-	public static int dimeEntero(String texto,Scanner sc) {
+	
+	public static int dimeEntero (String intStart, Scanner scanner) {
 		
-		do {
-		try {
-			System.out.println(texto);
-			String a = sc.nextLine();
-			int numero = Integer.parseInt(a);
-			return numero;
-			
+		while (true) {
+			System.out.println(intStart);
+			String entrada = scanner.nextLine();
+			try {
+				int entero = Integer.parseInt(entrada);
+				return entero;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Error");	
+			}
 		}
-		catch (Exception e) {System.out.println("Pon un numero para escoger un menu: ");}
-		}
-		while(true);
 	}
+	
+	public static void menuLongitud(Scanner scanner) {
+        int option;
+        do {
+            System.out.println("Selecciona la conversión de longitud:");
+            System.out.println("1. Millas a Kilómetros");
+            System.out.println("2. Kilómetros a Millas");
+            System.out.println("0. Salir");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.print("Ingrese millas: ");
+                    double millas = scanner.nextDouble();
+                    System.out.println(millas + " millas = " + millasAKilometros(millas) + " kilómetros");
+                    break;
+                case 2:
+                    System.out.print("Ingrese kilómetros: ");
+                    double kilometros = scanner.nextDouble();
+                    System.out.println(kilometros + " kilómetros = " + kilometrosAMillas(kilometros) + " millas");
+                    break;
+                case 0:
+                    System.out.println("Volviendo al menú principal.");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intente de nuevo.");
+            }
+        } while (option != 0);
+    }
+	
+	public static void menuPeso(Scanner scanner) {
+        int option;
+        do {
+            System.out.println("Selecciona la conversión de peso:");
+            System.out.println("1. Libras a Kilogramos");
+            System.out.println("2. Kilogramos a Libras");
+            System.out.println("0. Salir");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.print("Ingrese libras: ");
+                    double libras = scanner.nextDouble();
+                    System.out.println(libras + " libras = " + librasAKilogramos(libras) + " kilogramos");
+                    break;
+                case 2:
+                    System.out.print("Ingrese kilogramos: ");
+                    double kilogramos = scanner.nextDouble();
+                    System.out.println(kilogramos + " kilogramos = " + kilogramosALibras(kilogramos) + " libras");
+                    break;
+                case 0:
+                    System.out.println("Volviendo al menú principal.");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intente de nuevo.");
+            }
+        } while (option != 0);
+    }
+	
+	public static double millasAKilometros(double millas) {
+        return millas * 1.60934;
+    }
+
+    public static double kilometrosAMillas(double kilometros) {
+        return kilometros * 0.621371;
+    }
+
+    public static double librasAKilogramos(double libras) {
+        return libras * 0.453592;
+    }
+
+    public static double kilogramosALibras(double kilogramos) {
+        return kilogramos * 2.20462;
+    }
+
 }
+
--- reto2/src/reto2/retos.java
+++ reto2/src/reto2/retos.java
@@ -6,66 +6,31 @@ public class retos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner (System.in);
-		
-		int menu = funciones1.dimeEntero("Elige opción 1, 2 o 0", sc);
-		Scanner scanner = new Scanner (System.in);
-		boolean valido;
-		String a = sc.nextLine();
-		
-			switch (a) {
-			case 1: {
-				
-				do {
-					System.out.println("Selecciona el tipo de conversion:");
-					System.out.println("1. Longitud");
-					System.out.println("2. Peso");
-					System.out.println("3. Temperatura");
-					System.out.println("4. Volumen");
-					System.out.println("0. Salir");
-					String input = scanner.nextLine();
-					try {
-						menu = Integer.parseInt(input);
-						if (menu >=0 && menu<=4) {
-							valido = true;
-						} else {
-							System.out.println("N�mero incorrecto.");
-							valido = false;
-						}
-					} catch (Exception e) {
-						// TODO: handle exception
-						System.out.println("N�mero no reconocido.");
-						valido = false;
-					}
-				} while (!valido);
-				
-			}
-			case 2: {
-				do {
-					System.out.println("Selecciona el tipo de conversion de longitud:");
-					System.out.println("1. Millas a kilometros");
-					System.out.println("2. Kilometros a Millas");
-					System.out.println("0. Salir");
-					String input = scanner.nextLine();
-					try {
-						menu = Integer.parseInt(input);
-						if (menu >=0 && menu<=4) {
-							valido = true;
-						} else {
-							System.out.println("N�mero incorrecto.");
-							valido = false;
-						}
-					} catch (Exception e) {
-						// TODO: handle exception
-						System.out.println("N�mero no reconocido.");
-						valido = false;
-					}
-				} while (!valido);
-			}
-			default:
-				break;
-			}
-			
-	
+		Scanner sc = new Scanner(System.in);
+        int opcion;
+        
+        int menus = funciones1.dimeEntero("Selecciona el tipo de conversión:\n1 - Opicon 1 \n2 - Opcion 2\n0 - Salir", sc);
+        
+        switch (menus) {
+        case 1: 
+        	do {
+                opcion = funciones1.dimeEntero("Selecciona el tipo de conversión:\n1 - Longitud\n2 - Peso\n3 - Temperatura\n4 - Volumen\n0 - Salir", sc);
+
+                switch (opcion) {
+                    case 1:
+                        funciones1.menuLongitud(sc);
+                        break;
+                    case 2:
+                        funciones1.menuPeso(sc);
+                        break;
+                }
+            } while (opcion != 0);
+        default:
+        	break;
+        
+
+      
+    }
+        sc.close();
 	}
-}
+	}
